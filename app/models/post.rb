cf7271e7827989724aeb3a387f7a1528319cc8ba
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-08-25 20:18:33 -0700

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,10 @@ class Post < ApplicationRecord
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validate :not_to_allow_true_facts
+  validate :not_to_allow_to_be
 
-  def not_to_allow_true_facts
-    if title.present? && title == "True Facts"
-      errors.add(:title, "not to allow to be True Facts")
-    end
+  def not_to_allow_to_be
+    errors.add(:title, "not to allow to be") unless title.present? && (["Won't Believe", "Secret", "Guess"].any? { |s| title.include?(s)} || /Top\b\s\d+\b/.match?(title))
   end
 
 end
